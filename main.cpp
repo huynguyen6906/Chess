39a3e45b80dd379df6d1e8b91899c05a0ae18ac3
huynguyen6906 <dinhhuy6906@gmail.com>
2025-02-23 13:08:15 +0700

    Remove <iostream>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <SDL2/SDL.h>
-#include <iostream>
 
 int main(int argc, char* argv[]) {
     // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
@@ -15,7 +13,6 @@ int main(int argc, char* argv[]) {
                                           800, 600, 
                                           SDL_WINDOW_SHOWN);
     if (window == nullptr) {
-        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
@@ -23,7 +20,6 @@ int main(int argc, char* argv[]) {
     // Create a renderer
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (renderer == nullptr) {
-        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
         SDL_DestroyWindow(window);
         SDL_Quit();
         return 1;
