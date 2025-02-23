ad41ae65ab38ee1022d7779b198db5beb1f15830
huynguyen6906 <dinhhuy6906@gmail.com>
2025-02-23 12:58:49 +0700

    Test SDL 2 run or not

--- main.cpp
+++ main.cpp
@@ -1,7 +1,58 @@
 #include <SDL2/SDL.h>
 #include <iostream>
-using namespace  std;
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("SDL2 Window", 
+                                          SDL_WINDOWPOS_CENTERED, 
+                                          SDL_WINDOWPOS_CENTERED, 
+                                          800, 600, 
+                                          SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    // Create a renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == nullptr) {
+        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Main loop flag
+    bool running = true;
+    SDL_Event event;
+
+    // Main loop
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        // Clear the screen
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // White color
+        SDL_RenderClear(renderer);
+
+        // Present the renderer
+        SDL_RenderPresent(renderer);
+    }
+
+    // Clean up
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
